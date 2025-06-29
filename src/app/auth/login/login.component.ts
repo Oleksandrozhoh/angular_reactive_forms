84cc75992ffecea041e48b7a29cbbe334144a29b
Oleksandrozhoh <oleksandrozhoh@gmail.com>
2025-06-29 07:41:49 -0500

    reactive log in form development is complete

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,27 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounceTime, of } from 'rxjs';
+
+// custom validator function
+export function isContainQuestionMark(control: AbstractControl){
+  return control.value.includes("?") ? null : { doesNotContainQuestionMark: true}
+}
+// custom async validator function
+export function isEmailUnique(control: AbstractControl){
+  if(!['test@gmail.com'].includes(control.value)){
+    return of(null);
+  }
+  return of({notUnique: true})
+}
+
+// load email from local storage if there is a catched one there
+let initialEmailValue = '';
+const catchedEmail = window.localStorage.getItem('enteredEmail');
+if(catchedEmail){
+  const emailData = JSON.parse(catchedEmail);
+  initialEmailValue = emailData.email;
+}
+
 
 @Component({
   selector: 'app-login',
@@ -9,9 +31,10 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private destroyRef = inject(DestroyRef);
   form = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    email: new FormControl(initialEmailValue, {validators: [Validators.required, Validators.email], asyncValidators: [isEmailUnique]}),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), isContainQuestionMark]),
   });;
 
   get isEmailInvalid(){
@@ -20,6 +43,17 @@ export class LoginComponent {
 
   get isPasswordInvalid(){
     return this.form.controls.password.invalid && this.form.controls.password.touched && this.form.controls.password.dirty;
+  }
+
+  ngOnInit(){
+
+    const subscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: (enteredData)=>{
+        window.localStorage.setItem('enteredEmail', JSON.stringify({'email': enteredData.email}))
+      }
+    });
+
+    this.destroyRef.onDestroy(()=> subscription.unsubscribe());
   }
   
   onSubmit(){
